cdf37c4e281b6eb49be4b4107c542907803716fd
codeworkx <codeworkx@cyanogenmod.org>
2016-11-09 20:27:54 +0100

    healthd: Add support for BMS and WiPower.
    Battery node has no batteryFullChargePath and batteryCycleCountPath,
    but battery management system does. Simply add it.
    
    Also add support for QCOM WiPower chargers.
    
    Change-Id: I5fbbebb01cbe7c38af1f0997a832c79fcf42a098

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -187,6 +187,7 @@ BatteryMonitor::PowerSupplyType BatteryMonitor::readPowerSupplyType(const String
     static SysfsStringEnumMap<int> supplyTypeMap[] = {
             {"Unknown", ANDROID_POWER_SUPPLY_TYPE_UNKNOWN},
             {"Battery", ANDROID_POWER_SUPPLY_TYPE_BATTERY},
+            {"BMS", ANDROID_POWER_SUPPLY_TYPE_BATTERY},
             {"UPS", ANDROID_POWER_SUPPLY_TYPE_AC},
             {"Mains", ANDROID_POWER_SUPPLY_TYPE_AC},
             {"USB", ANDROID_POWER_SUPPLY_TYPE_USB},
@@ -198,6 +199,7 @@ BatteryMonitor::PowerSupplyType BatteryMonitor::readPowerSupplyType(const String
             {"USB_C", ANDROID_POWER_SUPPLY_TYPE_AC},
             {"USB_PD", ANDROID_POWER_SUPPLY_TYPE_AC},
             {"USB_PD_DRP", ANDROID_POWER_SUPPLY_TYPE_USB},
+            {"Wipower", ANDROID_POWER_SUPPLY_TYPE_WIRELESS},
             {"Wireless", ANDROID_POWER_SUPPLY_TYPE_WIRELESS},
             {"DASH", ANDROID_POWER_SUPPLY_TYPE_AC},
             {NULL, 0},
